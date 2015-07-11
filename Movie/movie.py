62b9a2a23955a9d93b69da5c759c3ac316b8eff5
Minh-Long Do <minhlong94@yahoo.de>
2015-07-11 19:37:52 +0200

    Show trailer function for  Movie class - Opens web browser  to run trailer from youtube (provided by given link)

--- Movie/movie.py
+++ Movie/movie.py
@@ -1,5 +1,7 @@
 __author__ = 'minhlongdo'
 
+import webbrowser
+
 
 class Movie:
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -7,3 +9,6 @@ class Movie:
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
