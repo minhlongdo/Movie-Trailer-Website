a3f0eb1ed0216590abc51bf1b1179698234040af
Minh-Long Do <minhlong94@yahoo.de>
2015-07-13 09:37:27 +0200

    Commented code

--- movie.py
+++ movie.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+"""
+Class Movie encapsulates the movie's title, storyline, poster image url and youtube trailer so it can be accessed
+by fresh_tomatoes.py to construct a website of movie trailers.
+"""
+
 __author__ = 'minhlongdo'
 
 import webbrowser
@@ -5,10 +12,17 @@ import webbrowser
 
 class Movie:
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """
+        Construct a movie object that contains the movie's title, storyline, URL of poster's image and trailer.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        Opens URL of the trailer on youtube.
+        :return: None
+        """
         webbrowser.open(self.trailer_youtube_url)
